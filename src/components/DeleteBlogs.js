c106b35340a11457a44a9acf62665eb48d60ae30
ericmoore1 <mooreericbernie@gmail.com>
2019-04-14 22:51:37 -0400

    getting items to delete

--- src/components/DeleteBlogs.js
+++ src/components/DeleteBlogs.js
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import Slide from '@material-ui/core/Slide';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
 import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+const Transition = (props) => {
+  return <Slide direction="up" {...props} />;
+}
+
+const DELETE_BLOGS = ( blogs ) => {
+    const blogsArray = Object.keys(blogs);
+    if( blogsArray.length === 0 ){return;}
+    console.log(blogsArray);
+}
+
 const DeleteBlogs = (props) => {
   // Declare a new state variable, which we'll call "count"
   const [itemsToDelete, setItemsToDelete] = useState({});
-  console.log("items to:  ",itemsToDelete)
   let _userBlogs = props.userBlogs || [];
   let _toggleParent = props.toggleParent;
+  let _isDelete = props.isDelete;
+  let _setIsDelete = props.setIsDelete;
+  let _setItemsToDelete = setItemsToDelete;
   return (
+
+    <Dialog
+         open={_isDelete}
+         TransitionComponent={Transition}
+         onClose={_setIsDelete}
+         aria-labelledby="alert-dialog-slide-title"
+         aria-describedby="alert-dialog-slide-description"
+       >
+       <DialogTitle id="alert-dialog-slide-title">
+           {"Select items for deletion"}
+       </DialogTitle>
+       <DialogContent>
     <div style={{ width: "80%",height : 250, overFlow : "hidden"}}>
        { _userBlogs.map( (o,i) => {
          let style = (o.id in itemsToDelete) ? {color : "red"} : { color : "black"};
@@ -28,6 +59,16 @@ const DeleteBlogs = (props) => {
          { o.title}</div>
        })}
     </div>
+    </DialogContent>
+    <DialogActions>
+        <Button onClick={() => {_setItemsToDelete({}); _setIsDelete();}} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={()=>{DELETE_BLOGS(itemsToDelete);_setItemsToDelete({}); _setIsDelete();}} color="primary">
+          Delete Selected Items
+        </Button>
+    </DialogActions>
+  </Dialog>
   );
 }
 
--- src/views/Blogs.js
+++ src/views/Blogs.js
@@ -3,12 +3,6 @@ import {Header,BlogCard,DeleteBlogs} from '../components/index';
 import {BlogsAPI} from '../api/index';
 import Button from '@material-ui/core/Button';
 
-import Dialog from '@material-ui/core/Dialog';
-import Slide from '@material-ui/core/Slide';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-
 const GET_BLOGS = BlogsAPI.getBlogs;
 const BLOG_CARDS = (canedit) => ( blogs ) =>  blogs.map( ( blog, index ) =>  <BlogCard canedit={canedit} key={blog.id} {...blog} />);
 const BlogButton = ( props) => { return props.bool ? <div  style={{margin : '1% 4%', textAlign : 'left'}}>
@@ -22,9 +16,7 @@ const BlogButton = ( props) => { return props.bool ? <div  style={{margin : '1% 
                   onClick={props.deleteFunct}>{props.deleteTitle}</Button>
                 </div> : null }
 
-  const Transition = (props) => {
-    return <Slide direction="up" {...props} />;
-  }
+
 const Blogs = (props) => {
   // Declare a new state variable, which we'll call "count"
   const [blogs, setBlogs] = useState([]);
@@ -56,42 +48,22 @@ const Blogs = (props) => {
 
 
   let _blogButton = BlogButton(_theProps);
+
   let _myBlogs = blogs.filter( blog => blog.user_id === userId);
   let _blogs = isUserBlogs ? _myBlogs : blogs;
-  let _setIsDelete = () => setIsDelete(false);
-  let _setCount = () => setCount(0);
+
+  let _props = {
+      userBlogs : _myBlogs,
+      toggleParent : () => setCount(0),
+      isDelete :  isDelete,
+      setIsDelete : () => setIsDelete(false)
+  };
   return (
     <div>
      <Header title="Blogs" />
      {_blogButton}
-     <div>
       {BLOG_CARDS(isUserBlogs)(_blogs)}
-     </div>
-
-     <Dialog
-          open={isDelete}
-          TransitionComponent={Transition}
-
-          onClose={_setIsDelete}
-          aria-labelledby="alert-dialog-slide-title"
-          aria-describedby="alert-dialog-slide-description"
-        >
-
-        <DialogTitle id="alert-dialog-slide-title">
-            {"Select items for deletion"}
-        </DialogTitle>
-        <DialogContent>
-                   <DeleteBlogs userBlogs={_myBlogs} toggleParent={_setCount}/>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={_setIsDelete} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={()=>{}} color="primary">
-              Delete Selected Items
-            </Button>
-        </DialogActions>
-      </Dialog>
+       <DeleteBlogs {..._props}/>
     </div>
   );
 }
